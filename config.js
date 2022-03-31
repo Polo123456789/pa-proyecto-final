43035774563621228e758e9c3211d4deaf788a5f
polo123456789 <pabsan02@gmail.com>
2022-03-31 17:53:51 -0600

    (+) Added mysql2

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        name: "pa_proyecto_final",
+        username: process.env['MYSQL_USER'],
+        password: process.env['MYSQL_PASSWORD'],
+        host: 'localhost',
+    }
+}
